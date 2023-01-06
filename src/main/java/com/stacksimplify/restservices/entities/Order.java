9bd82c4aead12929148c193db490552f1d4692df
LuisPeralta87 <lperaltaortiz@deloitte.com>
2023-01-06 01:18:49 -0600

    OneToMany SecondCommit

---
+++ src/main/java/com/stacksimplify/restservices/entities/Order.java
@@ -0,0 +1,55 @@
+package com.stacksimplify.restservices.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="orders")
+public class Order {
+
+	@Id
+	@GeneratedValue
+	private Long orderId;
+	private String orderDescription;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JsonIgnore
+	private Users user;
+
+	public Order() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getOrderDescription() {
+		return orderDescription;
+	}
+
+	public void setOrderDescription(String orderDescription) {
+		this.orderDescription = orderDescription;
+	}
+
+	public Users getUser() {
+		return user;
+	}
+
+	public void setUser(Users user) {
+		this.user = user;
+	}
+	
+	
+}
